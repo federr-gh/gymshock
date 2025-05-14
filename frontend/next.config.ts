d144f1d65bb11a26fe8b52c026d9a4ee7463e55b
Federico Guarín <federico.guarin@dataenthusiast.dev>
2025-05-14 00:08:06 -0500

    load gifs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['ejerciciosdb.com', 'rapidapi.com', 'i.ytimg.com', 'i9.ytimg.com'],
+    domains: ['ejerciciosdb.com', 'rapidapi.com', 'i.ytimg.com', 'i9.ytimg.com', 'gymvisual.com'],
     remotePatterns: [
       {
         protocol: 'https',
